ace1391d3f39545e845c12d0d76d8b5949539207
LunaXVee <lunamajaha@gmail.com>
2025-09-15 08:09:27 +0200

    Fixed my main section with my picture

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,22 +26,23 @@ export default function Navbar() {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+      <li><a>Home</a></li>
       <li>
         <details>
-          <summary>Parent</summary>
+          <summary>Contact</summary>
           <ul className="p-2">
             <li><a>Submenu 1</a></li>
             <li><a>Submenu 2</a></li>
           </ul>
         </details>
       </li>
-      <li><a>Item 3</a></li>
+      <li><a>Projects</a></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn ">Lets Connect</a>
   </div>
 </div>
  );
 }
+
