e11bd0da0d97ce32362e9610098591c788320e0f
LunaXVee <lunamajaha@gmail.com>
2025-09-16 13:44:39 +0200

    completed final styling version of about me section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,36 +2,28 @@ export default function Navbar() {
     return (
   
 
-<div className="navbar bg-custom shadow-sm z-1">
-<div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden bg-button">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+<div className="navbar bg-white shadow-sm mb-3 ">
+  <div className="flex-none">
+  <div className="dropdown">
+  <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
       </div>
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow ">
-        <li><a>Home</a></li>
-        <li><a>Projects</a></li>
-        <li><a>Resume/CV</a></li>
-        <li><a>Contact </a></li>
+        className="menu menu-sm dropdown-content bg-white rounded-box z-1 mt-3 w-52 p-2 shadow" style={{ fontSize: '22rem' }}>
+        <li><a className="text-base">Homepage</a></li>
+        <li><a className="text-base">About Me</a></li>
+        <li><a className="text-base">Projects</a></li>
+        <li><a className="text-base">Contact</a></li>
+
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">LunaXVee</a>
+    </div>
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl text-[#0A3044]">LunaXVee</a>
   </div>
- 
- 
-  <div className="navbar-center hidden lg:flex text-[#0A3044]">
-    <ul className="menu menu-horizontal px-1">
-      <li><a >Home</a></li>
-      <li><a className="font-bold">About Me</a></li>
-      <li><a>Projects</a></li>
-      <li><a>Contact</a></li>
-
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn  bg-button border-0 m-12">Download CV</a>
+  <div className="navbar-end mr-5 ">
+    <a className="btn" style={{ backgroundColor: '#0A3044' }}>Download Resume/CV</a>
   </div>
 </div>
  );
